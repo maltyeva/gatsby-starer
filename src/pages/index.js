2f925114ef8b5a894801e4671ce8f20aac0575a9
Maria <maria.altyeva@gmail.com>
2020-04-29 18:10:50 +0800

    added contact page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
-import React from "react"
+import React from "react";
+import { Link } from 'gatsby';
 
 export default () => (
   <div style={{color: `purple`}}>
+  <Link to="/contact">Contact</Link>
   <h1>Hi world!</h1>
   <p>What a world</p>
   <img src="https://source.unsplash.com/random/400x200" alt="" />
