de8f57017b68d0df6acb2233e1cb57440563b514
Maria <maria.altyeva@gmail.com>
2020-05-11 18:40:51 +0800

    addinglayout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 export default () => (
-  <div style={{margin: `3rem auto`, maxWidth: 600}}>
+  <Layout>
   <Link to="/contact">Contact</Link>
   <h1>Hi world!</h1>
   <p>What a world</p>
   <img src="https://source.unsplash.com/random/400x200" alt="" />
-  </div>
+  </Layout>
   )
